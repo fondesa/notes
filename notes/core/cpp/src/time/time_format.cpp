c901b46975fb79384018aedf07eb077a67843485
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-03-07 22:25:56 +0100

    Update inline docs

--- notes/core/cpp/src/time/time_format.cpp
+++ notes/core/cpp/src/time/time_format.cpp
@@ -4,9 +4,9 @@
 namespace Time::Format {
 
 ISO_8601 format(std::time_t time) {
-  // Create a buffer of the same result's size.
+  // Creates a buffer of the same result's size.
   char timeBuffer[sizeof "0000-00-00T00:00:00Z"];
-  // Convert the time in seconds to the ISO-8601 format.
+  // Converts the time in seconds to the ISO-8601 format.
   strftime(timeBuffer, sizeof timeBuffer, "%FT%TZ", gmtime(&time));
   return timeBuffer;
 }
@@ -15,9 +15,9 @@ std::time_t parse(ISO_8601 formattedDate) {
   auto movedDate = std::move(formattedDate);
   tm tm{};
   auto ss = std::istringstream(movedDate);
-  // Fill the time struct reading the ISO-8601 time.
+  // Fills the time struct reading the ISO-8601 time.
   ss >> std::get_time(&tm, "%Y-%m-%dT%TZ");
-  // Get the time_t value for the struct.
+  // Gets the time_t value for the struct.
   return timegm(&tm);
 }
 }  // namespace Time::Format
