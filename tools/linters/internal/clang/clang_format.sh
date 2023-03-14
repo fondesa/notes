1edf851e1df707fe03258f3896d5a8212f4ffb9d
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-03-14 23:32:41 +0100

    Add clang-format implementation

--- tools/linters/internal/clang/clang_format.sh
+++ tools/linters/internal/clang/clang_format.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-__main() {
+main() {
   # TODO support multiple clang-format versions
   if ! command -v clang-format-15 >/dev/null 2>&1; then
-    # TODO
-    echo "FAILURE"
+    echo "clang-format-15 is required but not installed"
     exit 1
   fi
-  # TODO
-  echo "SUCCESS"
+
+  (cd "$BUILD_WORKSPACE_DIRECTORY" && exec_clang_format)
 }
 
-__main
+exec_clang_format() {
+  local file_filter=("*.hpp" "*.cpp")
+  local cpp_files
+  cpp_files=$(git ls-files "${file_filter[@]}")
+
+  if [[ ${#cpp_files[@]} == 0 ]]; then
+    echo "Skipping clang-format since there are no files to check"
+    return
+  fi
+
+  # shellcheck disable=SC2086
+  clang-format-15 -i $cpp_files
+}
+
+main
