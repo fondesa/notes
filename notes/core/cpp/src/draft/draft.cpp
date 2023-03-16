a6771e6fc94d30fe416ed22dac0d40dfbb05a329
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-03-17 00:29:58 +0100

    Add methods to draft

--- notes/core/cpp/src/draft/draft.cpp
+++ notes/core/cpp/src/draft/draft.cpp
@@ -1,6 +1,9 @@
 #include "draft/draft.hpp"
 #include <utility>
 
+Draft::Draft(): Draft("", "") {
+}
+
 Draft::Draft(std::string title, std::string description) {
   this->title = std::move(title);
   this->description = std::move(description);
@@ -12,4 +15,12 @@ std::string Draft::getTitle() const {
 
 std::string Draft::getDescription() const {
   return description;
+}
+
+void Draft::setTitle(std::string newTitle) {
+  this->title = std::move(newTitle);
+}
+
+void Draft::setDescription(std::string newDescription) {
+  this->description = std::move(newDescription);
 }
--- notes/core/cpp/src/draft/draft.hpp
+++ notes/core/cpp/src/draft/draft.hpp
@@ -4,11 +4,16 @@
 
 class Draft {
  public:
+  Draft();
   Draft(std::string title, std::string description);
 
   [[nodiscard]] std::string getTitle() const;
 
   [[nodiscard]] std::string getDescription() const;
+
+  void setTitle(std::string newTitle);
+
+  void setDescription(std::string newDescription);
 
  private:
   std::string title;
